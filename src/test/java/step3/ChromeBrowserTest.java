b9f0f28a63ac03e76b2e376f786705e2953f9293
Sergey Puyandaykin <s.puyandaykin@javad.com>
2017-12-04 15:43:09 +0300

    chrome browser test

---
+++ src/test/java/step3/ChromeBrowserTest.java
@@ -0,0 +1,47 @@
+package step3;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ChromeBrowserTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+//        DesiredCapabilities caps = new DesiredCapabilities();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+//        caps.setCapability(ChromeOptions.CAPABILITY, options);
+//        caps.setCapability("unexpectedAlertBehaviour", "dismiss");
+//        driver = new ChromeDriver(caps);
+        driver = new ChromeDriver(options);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void myFirstTest(){
+        driver.get("http://localhost/litecart/admin");
+/*        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+        Assert.assertTrue(driver.getTitle().equals("My Store"));
+*/    }
+
+    @After
+    public void stop (){
+        driver.quit();
+        driver = null;
+    }
+}
